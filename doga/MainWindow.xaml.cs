8d9281677374b4ebad44f50850101bfd2caed21a
Dholhos <113104720+Dholhos@users.noreply.github.com>
2023-04-20 12:55:00 +0200

    Add files via upload

---
+++ doga/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace teszt
+{
+
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Tanulo> cuccok = new ObservableCollection<Tanulo>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
